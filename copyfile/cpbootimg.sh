9771578711b33f1c90d0c0a919bae0203dbbf38c
zhukunhua <zhu.kunhua@geniatech.com>
2018-04-14 17:10:16 +0800

    ADD: 添加以太网文件、更改拷贝文件命令

--- copyfile/cpbootimg.sh
+++ copyfile/cpbootimg.sh
@@ -6,4 +6,4 @@ scp jsh@192.168.1.5:/data/jsh/tmp/s905x_b/out/target/product/stvs9/boot.img .
 cp boot.img /media/pumpkin/50B6-F941/
 ls -l /media/pumpkin/50B6-F941/boot.img
 umount /media/pumpkin/50B6-F941
-
+rm boot.img
